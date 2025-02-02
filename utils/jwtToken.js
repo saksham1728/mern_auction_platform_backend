a8b17f1901cac950b011f4fa54694feedefeeef9
saksham1728 <148770428+saksham1728@users.noreply.github.com>
2025-02-02 17:45:10 +0530

    Update jwtToken.js

--- utils/jwtToken.js
+++ utils/jwtToken.js
@@ -9,6 +9,8 @@ export const generateToken = (user, message, statuscode, res) => {
         .cookie("token", token, {
             expires: new Date(Date.now() + cookieExpireDays * 24 * 60 * 60 * 1000),
             httpOnly: true,
+            secure:true,
+            sameSite:"None"
         })
         .json({
             success: true,
